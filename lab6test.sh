c271f6071f3483f293a8540012859ace5353ac22
Darshan Parajuli <parajulidarshan@gmail.com>
2017-03-15 22:02:49 -0700

    update

--- lab6test.sh
+++ lab6test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-FILE=../lab6/test/crux/test_files/$1
+FILE=lab6/test/crux/test_files/$1
 echo $FILE
-java -jar lab6.jar $FILE.crx
+java -jar lab6/lab6.jar $FILE.crx
 diff -a <(cat ${FILE%.crx}.in  | spim -file ${FILE%.crx}.asm | tail -n +2) ${FILE%.crx}.out
 echo
