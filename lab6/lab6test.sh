0baa759fb9446d509acdb67b736d5a99b6061441
Darshan Parajuli <parajulidarshan@gmail.com>
2017-03-15 22:01:44 -0700

    update

--- lab6/lab6test.sh
+++ lab6/lab6test.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-cd build
-
-for FILE in $(ls ../lab6/test/crux/test_files/*.crx)
-do
-    echo $FILE
-    java -jar lab6.jar $FILE
-    diff -a <(cat ${FILE%.crx}.in  | spim -file ${FILE%.crx}.asm) ${FILE%.crx}.out
-    echo
-done
+FILE=../lab6/test/crux/test_files/$1
+echo $FILE
+java -jar lab6.jar $FILE.crx
+diff -a <(cat ${FILE%.crx}.in  | spim -file ${FILE%.crx}.asm | tail -n +2) ${FILE%.crx}.out
+echo
