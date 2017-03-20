99d2fbb173ce6e5bc3e15c84b49ece9222860bcb
Darshan Parajuli <parajulidarshan@gmail.com>
2017-03-20 16:38:47 -0700

    updated lab6test

--- lab6test.sh
+++ lab6test.sh
@@ -25,14 +25,17 @@ tests=(
 "test22"
 )
 
-echo Building...
 
-build_dir=./lab6/build
+build_dir=lab6/build
+test_dir=lab6/test/crux/test_files
+
+echo "> Building"
+
 rm -rf ${build_dir}
 mkdir -p ${build_dir}
 javac $(find ./lab6/src -name "*.java") -d ${build_dir}
 
-echo Running tests
+echo "> Running tests in "${test_dir}
 for i in "${tests[@]}"
 do
     if [[ "$1" != "" && "$1" != "$i" ]];
@@ -40,14 +43,14 @@ do
         continue
     fi
 
-    FILE=./lab6/test/crux/test_files/$i
+    FILE=${test_dir}/$i
     java -classpath ./lab6/build crux.Compiler  ${FILE}.crx
     result=$(diff -a <(cat ${FILE%.crx}.in  | spim -file ${FILE%.crx}.asm | tail -n +2) ${FILE%.crx}.out)
     if [[ -z ${result} ]]
     then
-        echo ${FILE}: PASS
+        echo ${FILE##*/}: PASS
     else
-        echo ${FILE}: FAIL
+        echo ${FILE##*/}: FAIL
         while read -r line; do
             echo "  $line"
         done <<< "${result}"
